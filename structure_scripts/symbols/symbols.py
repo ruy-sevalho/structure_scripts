24e3aa09394751709f3434a8e1769030086a822f
U3ZO <Hr38ke$9io>
2023-02-16 11:49:14 -0300

    Update symbols.py

---
+++ structure_scripts/symbols/symbols.py
@@ -0,0 +1,20 @@
+from sympy import var
+
+# Nominal strength
+Rn = var("R_n")
+# Ultimate stress
+Fu = var("F_u")
+# Yield Stress
+Fy = var("F_y")
+# Effective Net Area
+Ae = var("A_E")
+# Gross Area
+Ag = var("A_g")
+# Net Area Subjected to Shear
+Anv = var("A_nv")
+# Gross Area Subjected to Shear
+Agv = var("A_gv")
+# ASD safety factor
+Omega = var("Omega")
+# LRFD safety factor
+phi = var("phi")
